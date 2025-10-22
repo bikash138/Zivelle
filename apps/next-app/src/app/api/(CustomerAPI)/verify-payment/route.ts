9e23f53e5a997d526957605983023f0c0d2f55c7
Bikash Shaw <vshaw138@gmail.com>
2025-10-22 14:07:17 +0530

    feat: implement atomic transactions for order and payment verification

--- apps/next-app/src/app/api/(CustomerAPI)/verify-payment/route.ts
+++ apps/next-app/src/app/api/(CustomerAPI)/verify-payment/route.ts
@@ -21,36 +21,41 @@ export async function POST(req: NextRequest) {
       .digest("hex");
 
     if (generated_signature === razorpay_signature) {
-      await prisma.order.update({
-        where:{
-          razorpayOrderId: razorpay_order_id
-        },
-        data:{
-          paymentStatus: "Success",
-          razorpayPaymentId: razorpay_payment_id,
-          items: {
-            create: items.map((item: Item) => ({
-                itemId: item.id, 
-                sellerId: item.adminId,
-                quantity: item.quantity, 
-                price: item.price, 
-                size: item.selectedSize, 
-            })),
+      await prisma.$transaction(async(tx)=>{
+        await tx.order.update({
+          where:{
+            razorpayOrderId: razorpay_order_id
           },
-        }
+          data:{
+            paymentStatus: "Success",
+            razorpayPaymentId: razorpay_payment_id,
+            items: {
+              create: items.map((item: Item) => ({
+                  itemId: item.id, 
+                  sellerId: item.adminId,
+                  quantity: item.quantity, 
+                  price: item.price, 
+                  size: item.selectedSize, 
+              })),
+            },
+          }
+        })
       })
+      
       // Payment is verified
       return NextResponse.json({ success: true });
     } else {
       // Verification failed
-      await prisma.order.updateMany({
-        where:{
-          razorpayOrderId: razorpay_order_id
-        },
-        data:{
-          paymentStatus: "Failed",
-          razorpayPaymentId: razorpay_payment_id
-        }
+      await prisma.$transaction(async(tx)=>{
+        await tx.order.updateMany({
+          where:{
+            razorpayOrderId: razorpay_order_id
+          },
+          data:{
+            paymentStatus: "Failed",
+            razorpayPaymentId: razorpay_payment_id
+          }
+        })
       })
       return NextResponse.json({ 
         success: false, 
